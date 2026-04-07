140910be7997a067924843b5359348d81a1e7d82
Ali Abdullah <abbdullah@logxgroup.com>
2026-04-08 00:16:19 +0500

    Updated Settings

--- settings.py
+++ settings.py
@@ -1,26 +1,17 @@
-# Policy Violation Seller Central Page Links
-policy_violation_url_list = {
-    # 'other_violations': '/performance/account/health/product-policies?t=pcx',
-    'suspected_intellectual_property_violations': '/performance/detail/product-policies?t=brand-protection&ref=sp_st_dash_sus_intellectual_prop',
-    'intellectual_property_complaints': '/performance/detail/product-policies?t=intel&ref=sp_st_dash_pp_intellectual',
-    'product_authenticity_complaints': '/performance/detail/product-policies?t=auth&ref=sp_st_dash_pp_auth',
-    'product_condition_customer_complaints':'/performance/detail/product-policies?t=condition&ref=sp_st_dash_pp_condition',
-    'food_and_product_safety_issues':'/performance/detail/product-policies?t=safe&ref=sp_st_dash_pp_safe',
-    'listing_policy_violations':'/performance/detail/product-policies?t=list&ref=sp_st_dash_pp_listing',
-    'restricted_product_policy_violations': '/performance/detail/product-policies?t=restricted&ref=sp_st_dash_rp',
-    'customer_product_reviews_policy_violations': '/performance/detail/product-policies?t=abuse&ref=sp_st_dash_pp_abuse'
-}
-
-# token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDM2OTNjNGRkMDc4YTM2YzFjMjM5OTciLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2NGZhZDIyZmEyYTY3YzJiYTkxOGEzNGEifQ.mMewIE1EU06hoQAUeYP_RXL0FZZQMGBSkuXQXLEF8ds'
-# token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDI2ZmUxZjFkNGM5MTdhMmI1Y2ViNjYiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2OWM2YzA4N2JkOGIzY2ViZmU2MTFlYmMifQ.eGF73JBIy0pO2ixbGed_JGRNXJ1SbOjp4APYON2mCEw'
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDAxNDE5MGI0OWU0ZTU4ZTBhMjQ0YWMiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2ODgxZDZiYmY3YjM4NzkxNDJjOGJlYTkifQ.0NaNXMhN2GL-E-dt2-Kof_grRlMWZBOPhd6J9da-Qtw"
-
-a_z_claims = '/gp/guarantee-claims/home.html'
-
-base_url = 'https://sellercentral.amazon.com'
+import  os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-AMAZON_HOME = 'https://sellercentral.amazon.com/home'
+policy_violation_url_list = load_dotenv('policy_violation_url_list')
 
-LOCAL = False  # Set to False to use Google Sheets instead of stores.csv
-LOCAL_DB = False  # Set to False to use QuickBase; True to use local SQLite database
+token = load_dotenv('token')
+
+a_z_claims = load_dotenv('a_z_claims')
+
+base_url = load_dotenv('base_url')
+
+AMAZON_HOME = os.getenv('AMAZON_HOME')
+
+LOCAL = load_dotenv('LOCAL')
+LOCAL_DB = load_dotenv('LOCAL_DB')
